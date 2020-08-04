0a944d339c1e2867a85c8270ed4eec51f9e0f18c
mmaxgamma <63747188+mmaxgamma@users.noreply.github.com>
2020-08-04 12:39:40 -0400

    nswerChoices not working as expected...not storing classes as it should.

--- quizGame.js
+++ quizGame.js
@@ -1,5 +1,6 @@
 const quizContent = document.getElementById("quizContainer");
 const question = document.getElementById("question");
+const answerChoices = document.getElementsByClassName("answers");
 let answerOne = document.getElementById("answerOne");
 let answerTwo = document.getElementById("answerTwo");
 let answerThree = document.getElementById("answerThree");
@@ -13,6 +14,7 @@ const answerCheck = (userChoice, correctChoice) => {
         //CSS code to glow green
     }
 
+
     else {
         //CSS code to glow red 
         alert("Incorrect!");
@@ -24,11 +26,34 @@ const buildQuiz = () => {
     const questionAnswer = [{question: "What's 2+2?", answers: ['0', '4', '2', '3'], correctAnswer: '4'}, 
     {question: "What's 3+3?", answers: ['6', '4','2','3'], correctAnswer: '6'}];
 
-    for(i=0; i < questions.length; i++) {
-        
-    }
+    //For each question in the array...
+    //for(let i=0; i < questionAnswer.length; i++) {
+
+        //Overwrites current question and answer choices depending on the question being asked
+    question.innerHTML = questionAnswer[0].question;
+    answerOne.innerHTML = questionAnswer[0].answers[0];
+    answerTwo.innerHTML = questionAnswer[0].answers[1];
+    answerThree.innerHTML = questionAnswer[0].answers[2];
+    answerFour.innerHTML = questionAnswer[0].answers[3];
+    
+   /* answerChoices[0].addEventListener("click", function() {
+        console.log("I work");
+    }); */
 
 
+    //Checks answer once user selects one.
+    /*for(let j=0; j < answerChoices.length; j++) {
+        answerChoices[j].addEventListener("click", function() {
+            userChoice = answerChoices[j];
+            answerCheck(userChoice, questionAnswer[i].correctAnswer);
+        }
+    );    */
+}
+
+buildQuiz();
+console.log("working");
+for (i=0; i < answerChoices.length; i++) {
+    console.log(answerChoices[i]);
 }
 
 
