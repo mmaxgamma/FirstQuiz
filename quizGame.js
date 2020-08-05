502de9b5465a3c1109e8c30bd396f987440c5ccb
mmaxgamma <63747188+mmaxgamma@users.noreply.github.com>
2020-08-04 21:14:20 -0400

    managed to get answer check to work

--- quizGame.js
+++ quizGame.js
@@ -1,14 +1,13 @@
 const quizContent = document.getElementById("quizContainer");
 const question = document.getElementById("question");
-const answerChoices = document.getElementsByClassName("answers");
 let answerOne = document.getElementById("answerOne");
 let answerTwo = document.getElementById("answerTwo");
 let answerThree = document.getElementById("answerThree");
 let answerFour = document.getElementById("answerFour");
+let answerChoices = [answerOne, answerTwo, answerThree, answerFour];
 let score = 0;
-
 const answerCheck = (userChoice, correctChoice) => {
-    if (userChoice === correctChoice) {
+    if (userChoice == correctChoice) {
         score++;
         alert("Correct!");
         //CSS code to glow green
@@ -22,19 +21,35 @@ const answerCheck = (userChoice, correctChoice) => {
 }
 
 const buildQuiz = () => {
+    let index = 0;
+
     //Should I try and split up the question numbers?
-    const questionAnswer = [{question: "What's 2+2?", answers: ['0', '4', '2', '3'], correctAnswer: '4'}, 
+    const questionAnswer = [{question: "What's 2+2?", answers: ['0', '4', '6', '3'], correctAnswer: '4'}, 
     {question: "What's 3+3?", answers: ['6', '4','2','3'], correctAnswer: '6'}];
 
     //For each question in the array...
     //for(let i=0; i < questionAnswer.length; i++) {
 
         //Overwrites current question and answer choices depending on the question being asked
-    question.innerHTML = questionAnswer[0].question;
-    answerOne.innerHTML = questionAnswer[0].answers[0];
-    answerTwo.innerHTML = questionAnswer[0].answers[1];
-    answerThree.innerHTML = questionAnswer[0].answers[2];
-    answerFour.innerHTML = questionAnswer[0].answers[3];
+        question.innerHTML = questionAnswer[index].question;
+        answerOne.innerHTML = questionAnswer[index].answers[0];
+        answerTwo.innerHTML = questionAnswer[index].answers[1];
+        answerThree.innerHTML = questionAnswer[index].answers[2];
+        answerFour.innerHTML = questionAnswer[index].answers[3];
+        
+        for(let j=0; j < answerChoices.length; j++) {
+                answerChoices[j].addEventListener("click", function() {
+                    userChoice = answerChoices[j].innerHTML;
+                    console.log(userChoice);
+                    console.log(questionAnswer[index].correctAnswer);
+                    answerCheck(userChoice, questionAnswer[index].correctAnswer);                   
+                }
+            );    
+        }
+        index++;
+    //};
+
+    //answerChoices[0].addEventListener("click", console.log("Hey you clicked me!"))
     
    /* answerChoices[0].addEventListener("click", function() {
         console.log("I work");
@@ -42,19 +57,13 @@ const buildQuiz = () => {
 
 
     //Checks answer once user selects one.
-    /*for(let j=0; j < answerChoices.length; j++) {
-        answerChoices[j].addEventListener("click", function() {
-            userChoice = answerChoices[j];
-            answerCheck(userChoice, questionAnswer[i].correctAnswer);
-        }
-    );    */
+    
 }
 
 buildQuiz();
 console.log("working");
-for (i=0; i < answerChoices.length; i++) {
-    console.log(answerChoices[i]);
-}
+console.log(answerChoices); 
+
 
 
 
